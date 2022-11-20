75e2cae867d2bf529e8f5ebc3f88a419c59c8ba8
dj-nlx <dj.nlx@netlox.io>
2022-11-20 23:18:30 +0900

    more aggresive tcp garbage cleaner

--- common/llb_dpapi.h
+++ common/llb_dpapi.h
@@ -52,7 +52,7 @@
 #define CT_V4_CPTO            (1800000000000)
 
 /* Hard-timeouts for ct xxx entry */
-#define CT_TCP_FN_CPTO        (60000000000)
+#define CT_TCP_FN_CPTO        (10000000000)
 #define CT_SCTP_FN_CPTO       (60000000000)
 #define CT_UDP_FN_CPTO        (60000000000)
 #define CT_ICMP_FN_CPTO       (40000000000)
