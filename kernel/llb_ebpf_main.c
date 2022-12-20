d9a930669b54682c872d1dfcbe788ad324c3c85d
Dip J <dj.nlx@netlox.io>
2022-12-20 17:25:40 +0900

    config defs moved out

--- kernel/llb_ebpf_main.c
+++ kernel/llb_ebpf_main.c
@@ -10,10 +10,6 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
-#define HAVE_DP_FC 1
-#define HAVE_DP_EXTCT 1
-#define HAVE_DP_SCTP_SUM 1
-
 #include "llb_kern_entry.c"
 
 char _license[] SEC("license") = "Dual BSD/GPL";
