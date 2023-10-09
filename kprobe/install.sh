0a4a535de81934a4834da5618e383d38b4dce542
Trekkie <trekkie@netlox.io>
2023-10-09 12:09:57 +0900

    Experimental kprobe based csum flag fixup for loxilb processed packets

--- kprobe/install.sh
+++ kprobe/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# usage : curl -sfL https://github.com/loxilb-io/loxilb-ebpf/raw/main/kprobe/install.sh | sh -
 sudo apt-get install build-essential linux-headers-`uname -r`
 git clone -n --depth=1 --filter=tree:0 https://github.com/loxilb-io/loxilb-ebpf
 cd loxilb-ebpf
@@ -6,4 +7,5 @@ git sparse-checkout set --no-cone kprobe
 git checkout
 cd kprobe
 make
-sudo make install
+sudo make mod-install
+cd ../../
