a92ff60039e084acbe4760a182cf2f0a5cf6b976
Trekkie Coder <trekkie@netlox.io>
2024-03-27 19:38:53 +0900

    cgroup mount script modified

--- utils/mkllb_cgroup.sh
+++ utils/mkllb_cgroup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ "$#" -ne 0 ]; then
+  if [ ! -d /opt/loxilb/cgroup/ ]; then
+    mkdir -p /opt/loxilb/cgroup/ && mount -t cgroup2 -o rw,relatime,nsdelegate,memory_recursiveprot cgroup2 /opt/loxilb/cgroup/;
+  fi
+  exit 0
+fi
 if [ ! -d /opt/loxilb/cgroup/ ]; then
   mkdir -p /opt/loxilb/cgroup/ && mount -t cgroup2 -o rw,relatime,nsdelegate,memory_recursiveprot cgroup2 /opt/loxilb/cgroup/;
 else
