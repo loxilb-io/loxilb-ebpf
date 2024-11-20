4bf6e3e36ae7de560fe2cfe6811cb635333af979
Trekkie Coder <trekkie@netlox.io>
2024-11-20 11:49:19 +0900

    loxilb-io/loxilb#864 Updated mark values fro src-ranges

--- kernel/llb_kern_natlbfwd.c
+++ kernel/llb_kern_natlbfwd.c
@@ -138,7 +138,7 @@ dp_do_nat(void *ctx, struct xfi *xf)
   memset(&key, 0, sizeof(key));
   key.mark = xf->pm.dp_mark;
 
-  if (!(key.mark & 0x80000000)) {
+  if (!(key.mark & 0xc0000000)) {
     DP_XADDR_CP(key.daddr, xf->l34m.daddr);
     if (xf->l34m.nw_proto != IPPROTO_ICMP) {
       key.dport = xf->l34m.dest;
@@ -150,7 +150,6 @@ dp_do_nat(void *ctx, struct xfi *xf)
     if (xf->l2m.dl_type == bpf_ntohs(ETH_P_IPV6)) {
       key.v6 = 1;
     }
-    key.mark = 0;
   }
 
   LL_DBG_PRINTK("[NAT] Lookup");
