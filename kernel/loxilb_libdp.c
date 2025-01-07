5f963a445a600335bffb3b743766b51e7a625675
Trekkie Coder <trekkie@netlox.io>
2025-01-08 08:42:50 +0900

    map-fixes backported

--- kernel/loxilb_libdp.c
+++ kernel/loxilb_libdp.c
@@ -2056,9 +2056,11 @@ llb_fetch_pol_map_stats(int tid, uint32_t e, void *ppass, void *pdrop)
 void 
 llb_map_loop_and_delete(int tid, dp_map_walker_t cb, dp_map_ita_t *it)
 {
-  void *key = NULL;
+  void *pkey = NULL;
   llb_dp_map_t *t;
   uint32_t n = 0;
+  int delete = 0;
+  uint8_t key[1024];
 
   if (xh->have_noebpf) {
     return;
@@ -2071,22 +2073,43 @@ llb_map_loop_and_delete(int tid, dp_map_walker_t cb, dp_map_ita_t *it)
 
   t = &xh->maps[tid];
 
-  while (bpf_map_get_next_key(t->map_fd, key, it->next_key) == 0) {
-    if (n >= (10*t->max_entries)) break;
+  while (bpf_map_get_next_key(t->map_fd, pkey, it->next_key) == 0) {
+    if (n >= (t->max_entries)) break;
+
+    if (delete) {
+      llb_maptrace_uhook(tid, 0, pkey, it->key_sz, NULL, 0);
+      bpf_map_delete_elem(t->map_fd, pkey);
+    }
 
     if (bpf_map_lookup_elem(t->map_fd, it->next_key, it->val) != 0) {
       goto next;
     }
 
+    if (it->key_sz > 0) {
+      memcpy(key, it->next_key, it->key_sz);
+    } else {
+      memcpy(key, it->next_key, sizeof(key));
+    }
+
     if (cb(tid, it->next_key, it)) {
-      llb_maptrace_uhook(tid, 0, it->next_key, it->key_sz, NULL, 0);
-      bpf_map_delete_elem(t->map_fd, it->next_key);
+      delete = 1;
+    } else {
+      delete = 0;
     }
 
 next:
-    key = it->next_key;
+    pkey = key;
     n++;
   }
+
+  if (pkey != NULL && delete) {
+    llb_maptrace_uhook(tid, 0, pkey, it->key_sz, NULL, 0);
+    bpf_map_delete_elem(t->map_fd, pkey);
+  }
+
+#ifdef LLB_DP_CT_DEBUG
+  log_trace("TID %d entry loop count: %d", tid, n);
+#endif
 
   return;
 }
@@ -2366,6 +2389,8 @@ llb_dp_ufw42_pdiop(struct pdi_rule *new, struct dp_fwv4_ent *e)
   }
 }
 
+static void ll_map_ct_rm_any(void);
+
 int
 llb_add_mf_map_elem__(int tbl, void *k, void *v)
 {
@@ -2406,6 +2431,8 @@ llb_add_mf_map_elem__(int tbl, void *k, void *v)
       n++;
     }
     PDI_MAP_ULOCK(xh->ufw4);
+
+    if (ret == 0) ll_map_ct_rm_any();
   }
   return ret;
 }
@@ -2850,8 +2877,10 @@ ll_age_fcmap(void)
   fc_val = calloc(1, sizeof(*fc_val));
   if (!fc_val) return;
 
+  memset(&next_key, 0, sizeof(next_key));
   memset(&it, 0, sizeof(it));
   it.next_key = &next_key;
+  it.key_sz = sizeof(next_key);
   it.val = fc_val;
   it.uarg = &ns;
 
@@ -3128,12 +3157,14 @@ ll_ct_map_ent_has_aged(int tid, void *k, void *ita)
       llb_nat_dec_act_sessions(adat->ctd.rid, adat->ctd.aid);
     }
 
+#if 0
     if (!adat->ctd.pi.frag) {
       ll_send_ctep_reset(&xkey, &axdat);
       llb_maptrace_uhook(LL_DP_CT_MAP, 0, &xkey, sizeof(xkey), NULL, 0);
       bpf_map_delete_elem(t->map_fd, &xkey);
       llb_clear_map_stats(LL_DP_CT_STATS_MAP, axdat.ca.cidx);
     }
+#endif
     return 1;
   }
 
@@ -3289,6 +3320,24 @@ ll_ct_map_ent_rm_related(int tid, void *k, void *ita)
   return 0;
 }
 
+static int
+ll_ct_map_ent_rm_any(int tid, void *k, void *ita)
+{
+  struct dp_ct_key *key = k;
+  dp_map_ita_t *it = ita;
+  struct dp_ct_tact *adat;
+
+  if (!it|| !it->uarg || !it->val) return 0;
+
+  adat = it->val;
+
+  if (!key->v6) {
+    llb_del_map_elem_with_cidx(LL_DP_FCV4_MAP, adat->ca.cidx);
+  }
+  llb_clear_map_stats(LL_DP_CT_STATS_MAP, adat->ca.cidx);
+  return 1;
+}
+
 static void
 ll_map_ct_rm_related(uint32_t rid, uint32_t *aids, int naid)
 {
@@ -3312,6 +3361,7 @@ ll_map_ct_rm_related(uint32_t rid, uint32_t *aids, int naid)
 
   memset(&it, 0, sizeof(it));
   it.next_key = &next_key;
+  it.key_sz = sizeof(next_key);
   it.val = adat;
   it.uarg = as;
 
@@ -3325,6 +3375,39 @@ ll_map_ct_rm_related(uint32_t rid, uint32_t *aids, int naid)
   if (adat) free(adat);
   if (as) free(as);
 }
+
+static void
+ll_map_ct_rm_any(void)
+{
+  dp_map_ita_t it;
+  int i = 0;
+  struct dp_ct_key next_key;
+  struct dp_ct_tact *adat;
+  ct_arg_struct_t *as;
+  uint64_t ns = get_os_nsecs();
+
+  adat = calloc(1, sizeof(*adat));
+  if (!adat) return;
+
+  as = calloc(1, sizeof(*as));
+  if (!as) {
+    free(adat);
+    return;
+  }
+
+  as->curr_ns = ns;
+
+  memset(&it, 0, sizeof(it));
+  it.next_key = &next_key;
+  it.key_sz = sizeof(next_key);
+  it.val = adat;
+  it.uarg = as;
+
+  llb_map_loop_and_delete(LL_DP_CT_MAP, ll_ct_map_ent_rm_any, &it);
+  if (adat) free(adat);
+  if (as) free(as);
+}
+
 
 static void
 llb_set_rlims(void)
